80dec291f7c0abe378801d4e4d46f776bdcde52b
sergei-doroshenko <6823298@gmail.com>
2015-03-18 18:59:26 +0300

    oop 2

---
+++ go_courses/src/core/_3_io/_1_io/state/AppState.java
@@ -0,0 +1,41 @@
+package core._3_io._1_io.state;
+
+import java.io.*;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Arrays;
+import java.util.zip.GZIPOutputStream;
+
+/**
+ * Created by Sergei on 18.03.2015.
+ *
+ * Remove double bytes from byte array using STATE pattern
+ */
+public class AppState {
+    public static void main(String[] args) throws MalformedURLException {
+
+        byte[] bytes = {0, 0, 10, 12, 1, 3, 0, 0, 0, 4, 7, 1, 0, 0, 5, 8}; // 16
+
+        try(InputStream in = new ByteArrayInputStream(bytes);
+            ByteArrayOutputStream out = new ByteArrayOutputStream()
+        ) {
+            filter(in, out, 8);
+            byte[] b0 = out.toByteArray();
+            System.out.println(Arrays.toString(b0));
+        } catch (IOException e0) {
+            /* NOP */
+        }
+    }
+
+    public static void filter(InputStream src, OutputStream dst, int buffSize) throws IOException {
+        byte[] buff = new byte[buffSize];
+
+        while ( src.read(buff) != -1 ) {
+            State state = new Zero(dst);
+            for (int i = 0; i < buff.length; i++) {
+                state = state.next(buff[i]);
+            }
+            state.finish();
+        }
+    }
+}
---
+++ go_courses/src/core/_6_oop/_4_principles/composite/Romb.java
@@ -0,0 +1,15 @@
+package core._6_oop._4_principles.composite;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+/**
+ * Created by Sergei on 18.03.2015.
+ */
+public class Romb implements Figure {
+
+    @Override
+    public void draw() {
+        System.out.println("Romb");
+    }
+}
