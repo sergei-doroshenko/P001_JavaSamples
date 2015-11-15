69751aef9053bcd12c53deb80c98ac390196a9a4
Sergei <6823298@gmail.com>
2015-11-15 21:55:42 +0300

    Regex quantifiers. Merge remote-tracking branch 'origin/master'
    Conflicts:
    	.idea/compiler.xml
    	.idea/encodings.xml
    	.idea/misc.xml
    	.idea/modules.xml
    	go_courses/go_courses.iml
    	jcip/jcip.iml

--- 1Z0-804/src/_09_NIO2/_9_5_PathExists.java
+++ 1Z0-804/src/_09_NIO2/_9_5_PathExists.java
@@ -6,18 +6,15 @@ class _9_5_PathExists {
 	public static void main(String[] args) {
 		Path path = Paths.get(args[0]);
 		
-		if(Files.exists(path, LinkOption.NOFOLLOW_LINKS)) 
-		{
+		if(Files.exists(path, LinkOption.NOFOLLOW_LINKS)) {
 			System.out.println("The file/directory " + path.getFileName() + " exists");
 			// check whether it is a file or a directory
-			if(Files.isDirectory(path, LinkOption.NOFOLLOW_LINKS)) { 
+			if (Files.isDirectory(path, LinkOption.NOFOLLOW_LINKS)) {
 				System.out.println(path.getFileName() + " is a directory");
-			} 
-			else {
+			} else {
 				System.out.println(path.getFileName() + " is a file");
 			}
-		}
-		else { 
+		} else {
 			System.out.println("The file/directory " + path.getFileName() + " does not exist");
 		}
 	}
--- 1Z0-804/src/_09_NIO2/_9_7_FileAttributes.java
+++ 1Z0-804/src/_09_NIO2/_9_7_FileAttributes.java
@@ -7,19 +7,19 @@ class _9_7_FileAttributes {
 	public static void main(String[] args) {
 		Path path = Paths.get(args[0]);
 		try {
-Object object = Files.getAttribute(path, "creationTime", LinkOption.NOFOLLOW_LINKS);
+			Object object = Files.getAttribute(path, "creationTime", LinkOption.NOFOLLOW_LINKS);
 			System.out.println("Creation time: " + object);
 			
-object = Files.getAttribute(path, "lastModifiedTime", LinkOption.NOFOLLOW_LINKS);
+			object = Files.getAttribute(path, "lastModifiedTime", LinkOption.NOFOLLOW_LINKS);
 			System.out.println("Last modified time: " + object);
 			
-object = Files.getAttribute(path, "size", LinkOption.NOFOLLOW_LINKS);
+			object = Files.getAttribute(path, "size", LinkOption.NOFOLLOW_LINKS);
 			System.out.println("Size: " + object);
 			
-object = Files.getAttribute(path, "dos:hidden", LinkOption.NOFOLLOW_LINKS);
+			object = Files.getAttribute(path, "dos:hidden", LinkOption.NOFOLLOW_LINKS);
 			System.out.println("isHidden: " + object);
 			
-object = Files.getAttribute(path, "isDirectory", LinkOption.NOFOLLOW_LINKS);
+			object = Files.getAttribute(path, "isDirectory", LinkOption.NOFOLLOW_LINKS);
 			System.out.println("isDirectory: " + object);
 		} catch (IOException e) {
 			e.printStackTrace();
