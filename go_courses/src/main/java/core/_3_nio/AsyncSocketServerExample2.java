31e7f49c9e1c9d9afa64586d8eddeca7b2d53421
sergei.doroshenko <6823298@gmail.com>
2016-09-01 15:09:36 +0300

    Enhanced nio examples.

--- go_courses/src/main/java/core/_3_nio/AsyncSocketServerExample2.java
+++ go_courses/src/main/java/core/_3_nio/AsyncSocketServerExample2.java
@@ -11,7 +11,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ExecutionException;
-import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 
